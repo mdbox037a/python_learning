0667799c05d47aba0ee29a026cf4b605705a7aea
matt box <mdbox037a@protonmail.com>
2025-12-15 11:11:56 -0500

    create simple helper to open the current ubuntu kernel mailing list page

---
+++ experiments/kernel_ml.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+from typing import NoReturn
+
+
+def open_kernel_ml() -> NoReturn:
+    """
+    Opens the Ubuntu kernel team mailing list for the current month and year in a browser
+    """
+    try:
+        date = subprocess.run(["date", "+%Y %B"], capture_output=True, text=True)
+        now = date.stdout.replace(" ", "-")
+        ml_url = f"https://lists.ubuntu.com/archives/kernel-team/{now}/"
+        subprocess.Popen(
+            ["xdg-open", ml_url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    open_kernel_ml()
