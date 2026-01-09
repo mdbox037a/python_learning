3228e4526f2e72beb2510fc888e9c3ef63d509be
matt box <mdbox037a@protonmail.com>
2026-01-09 16:42:33 -0500

    complete draft 1 of recursive solution for running total

--- boot.dev/cart_total_recursive.py
+++ boot.dev/cart_total_recursive.py
@@ -1,2 +1,7 @@
-def cart_total_recursive(prices):
-    pass
+def cart_total_recursive(prices: list[int]):
+    total = 0
+    if len(prices) == 0:
+        return 0
+    else:
+        total += prices.pop(-1) + cart_total_recursive(prices)
+    return total
