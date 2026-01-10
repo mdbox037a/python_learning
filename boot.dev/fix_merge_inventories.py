a7495a5ae14a92a0b5c18209f1326b7a9843ae4b
matt box <mdbox037a@protonmail.com>
2026-01-10 08:15:33 -0500

    complete v1 answer to assignment

--- boot.dev/fix_merge_inventories.py
+++ boot.dev/fix_merge_inventories.py
@@ -1,5 +1,21 @@
 # Exercise: fix merge_inventories so that it correctly sums the quantities when the same item exists in both dictionaries.
-def merge_inventories(inv_a, inv_b):
+def original_merge_inventories(inv_a, inv_b):
+    result = {}
+
+    for item in inv_a:
+        result[item] = inv_a[item]
+
+    for item in inv_b:
+        result[item] = inv_b[item]
+
+    return result
+
+
+def fix_merge_inventories(inv_a: dict, inv_b: dict) -> dict:
+    """
+    return merged inventory dictionary from two input inventories dictionaries
+    if an item exists in both, the merged value should be the sum of the two input values
+    """
     result = {}
 
     for item in inv_a:
