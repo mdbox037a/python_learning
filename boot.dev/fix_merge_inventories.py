e957fefaa80f2f9894ad0cfc231412bb879e1d42
matt box <mdbox037a@protonmail.com>
2026-01-10 08:19:27 -0500

    refactor: start with shallow copy of a; use .items for checks

--- boot.dev/fix_merge_inventories.py
+++ boot.dev/fix_merge_inventories.py
@@ -16,12 +16,11 @@ def fix_merge_inventories(inv_a: dict, inv_b: dict) -> dict:
     return merged inventory dictionary from two input inventories dictionaries
     if an item exists in both, the merged value should be the sum of the two input values
     """
-    result = {}
-
-    for item in inv_a:
-        result[item] = inv_a[item]
-
-    for item in inv_b:
-        result[item] = inv_b[item]
+    result = inv_a.copy()
+    for item, quantity in inv_b.items():
+        if item in result:
+            result[item] += quantity
+        else:
+            result[item] = quantity
 
     return result
