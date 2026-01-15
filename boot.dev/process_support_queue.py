6d162bf06b57f0cc3aea9d087a6cac6a72963bf1
matt box <mdbox037a@protonmail.com>
2026-01-14 20:42:47 -0500

    complete quick daily exercise - processing customer support queue

---
+++ boot.dev/process_support_queue.py
@@ -0,0 +1,12 @@
+def process_support_queue(current_queue: list, new_customers: list, to_serve: int):
+    """current_queue: list of strings
+    new_customers: list of strings
+    to_serve: integer"""
+
+    working_queue = current_queue
+    working_queue.extend(new_customers)
+    served_customers = []
+    while to_serve > 0 and len(working_queue) > 0:
+        served_customers.append(working_queue.pop(0))
+        to_serve -= 1
+    return served_customers
