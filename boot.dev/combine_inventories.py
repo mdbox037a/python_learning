95b9a3649fecc4d7f2faf7ab582e9ede355ee0ba
matt box <mdbox037a@protonmail.com>
2026-01-27 20:10:56 -0500

    fix combine_inventories function to prevent clobbering of inv1 values by inv2 values

--- boot.dev/combine_inventories.py
+++ boot.dev/combine_inventories.py
@@ -1,3 +1,9 @@
-def combine_inventories(inv1, inv2):
-    inv1.update(inv2)
-    return inv1
+def combine_inventories(inv1: dict, inv2: dict) -> dict:
+    combined_inv = {}
+    combined_inv.update(inv1)
+    for item in inv2:
+        if item in combined_inv:
+            combined_inv[item] += inv2[item]
+        else:
+            combined_inv[item] = inv2[item]
+    return combined_inv
