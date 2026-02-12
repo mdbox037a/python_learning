206886d823b26770feae8e0d2a3d54830d28d79c
matt box <mdbox037a@protonmail.com>
2026-02-11 21:27:37 -0500

    add list comprehension solution to boost_scores.py

--- boot.dev/boost_scores.py
+++ boot.dev/boost_scores.py
@@ -5,3 +5,8 @@ def boost_scores(scores: list, bonus: int) -> list[int]:
         boosted_scores.append(score + bonus)
 
     return boosted_scores
+
+
+def another_boost_scores(scores: list, bonus: int) -> list[int]:
+    boosted_scores = [score + bonus for score in scores]
+    return boosted_scores
