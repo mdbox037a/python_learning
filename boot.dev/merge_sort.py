89a2ca6d6950f3f877be80cc2d8b0ca5a15faefd
matt box <mdbox037a@protonmail.com>
2026-02-12 20:51:02 -0500

    complete practice build of merge sort

---
+++ boot.dev/merge_sort.py
@@ -0,0 +1,27 @@
+def merge_sort(numbers):
+    if len(numbers) <= 1:
+        return numbers
+
+    mid = len(numbers) // 2
+    left = merge_sort(numbers[:mid])
+    right = merge_sort(numbers[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
