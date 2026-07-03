0cc303490c1a49573f49c7cd0911828312905cdf
Matt Box <mdbox037a@protonmail.com>
2026-07-03 19:52:40 -0400

    add rough consonant counting function

--- boot.dev/count_vowels.py
+++ boot.dev/count_vowels.py
@@ -7,3 +7,20 @@ def count_vowels(message: str) -> int:
         if char in vowels:
             num_vowels += 1
     return num_vowels
+
+
+def count_consonants(message: str) -> int:
+    lower_message = message.lower()
+    other = [
+        "!",
+        "?",
+        ".",
+        " ",
+    ]
+    vowels = ["a", "e", "i", "o", "u"]
+    num_consonants = 0
+
+    for char in lower_message:
+        if char not in vowels or char not in other:
+            num_consonants += 1
+    return num_consonants
