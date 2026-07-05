b5128e121efce766ba3a3450391526fa3b74a977
Matt Box <mdbox037a@protonmail.com>
2026-07-04 21:34:42 -0400

    add helper function to output contents of counts dict to user

--- boot.dev/tally_loot.py
+++ boot.dev/tally_loot.py
@@ -4,4 +4,10 @@ def tally_loot(loot_items):
         if item not in counts:
             counts[item] = 0
         counts[item] += 1
+    print_counts(counts)
     return counts
+
+def print_counts(counts: dict):
+    for item in counts:
+        print(f"{item}: {counts[item]}")
+    return
